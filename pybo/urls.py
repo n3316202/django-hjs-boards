6e4b41c3d41db8c4c8de88fe047f5a1415d0025a
heo <admin@admin.com>
2025-03-17 10:18:52 +0900

    commit

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from pybo import views
 
-
+## http://127.0.0.1:8000/pybo
 urlpatterns = [
     path("", views.index),
+    # http://127.0.0.1:8000/pybo/<int:question_id>/
+    path("<int:question_id>/", views.detail),  #  dev_3
 ]
