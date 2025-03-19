120a9fe738ec5d56b472d6b31d36b429bbddd9f7
heo <admin@admin.com>
2025-03-19 13:43:09 +0900

    commit

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from pybo import views
 
-# http://127.0.0.1:8000
+
+# http://127.0.0.1:8000/
 urlpatterns = [
     path("admin/", admin.site.urls),  # http://127.0.0.1:8000
     path("pybo/", include("pybo.urls")),
     path("common/", include("common.urls")),  # dev_13
+    path("", views.index, name="index"),  # dev_13
 ]
