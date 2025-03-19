0f3f7ad5c479ac9cb490d935ce1537644ac82329
heo <admin@admin.com>
2025-03-19 14:09:00 +0900

    commit

--- common/urls.py
+++ common/urls.py
@@ -2,6 +2,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
 
+from common import views
+
 app_name = "common"
 
-urlpatterns = [path("login/", auth_view.LoginView.as_view(template_name="common/login.html"), name="login")]  # dev_13
+urlpatterns = [
+    path(
+        "login/",
+        auth_view.LoginView.as_view(template_name="common/login.html"),
+        name="login",
+    ),  # dev_13
+    path("logout/", views.logout_view, name="logout"),
+]
