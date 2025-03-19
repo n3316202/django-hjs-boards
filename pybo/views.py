9b6f51bae0826d42ce8a01a4b670270860ffcfe0
heo <admin@admin.com>
2025-03-19 10:08:18 +0900

    commit

--- pybo/views.py
+++ pybo/views.py
@@ -5,21 +5,19 @@ from pybo.forms import AnswerForm, QuestionForm
 from pybo.models import Answer, Question
 from django.utils import timezone
 from django.core.paginator import Paginator
+
 # Create your views here.
 
 
 # http://127.0.0.1:8000/pybo/
 def index(request):
-    
-    page = request.Get.get('page','1') #페이지
-    
+
+    page = request.GET.get("page", "1")  # 페이지
     question_list = Question.objects.order_by("-create_date")
-    
-    paginator = Paginator(question_list,10) #페이지당 10개씩 보여주기
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'qustion_list':page_obj}
-    
-    #context = {"question_list": question_list}
+    context = {"question_list": page_obj}
+
     return render(request, "pybo/question_list.html", context)
 
 
@@ -34,26 +32,28 @@ def detail(request, question_id):
     # path(
     #     "answer/create/<int:question_id>/", views.answer_create, name="answer_create"
     # ),  # dev_5
+
+
 # dev_9
 def answer_create(request, question_id):
     """
     pybo 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    
+
     if request.method == "POST":
         form = AnswerForm(request.POST)
-        
+
         if form.is_valid():
             answer = form.save(commit=False)
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect("pybo:detail", question_id=question.id)
-        
+
     else:
         return HttpResponseNotAllowed("Only POST is possible.")
-    
+
     context = {"question": question, "form": form}
     return render(request, "pybo/question_detail.html", context)
 
