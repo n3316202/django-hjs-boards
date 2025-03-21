7aade6c9a22b99542da17d2e94a987cc95522cbe
heo <admin@admin.com>
2025-03-21 10:01:40 +0900

    commit

--- pybo/urls.py
+++ pybo/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path("set-session/", views.set_session_view, name="set_session"),
     path("get-session/", views.get_session_view, name="get_session"),
     path("delete-session/", views.delete_session_view, name="delete_session"),
+    
+    
 ]
